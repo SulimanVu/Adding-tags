bfd5e4ea6eb51cfd5a9f3d68291563d3d8655145
Suliman <100429884+SulimanVu@users.noreply.github.com>
2022-08-08 16:48:30 +0300

    добавление тэгов

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import './index.css';
 
 function App() {
+
+  const [text, setText] = useState('')
+  const [tag, setTag] = useState([])
+  const [error, setError] = useState('')
+
+  let newArr = tag
+  function hendleChange(e){
+      setText(e.target.value)
+      setError('')
+      e.target.style.boxShadow='none'
+  }
+  function hendleSend(){
+      setTag([...tag, text])
+      setText('')
+  }
+
+  function hendleOnBlur(e){
+    if(!e.target.value){
+      setError('Поле ввода не должно быть пустым')
+      e.target.style.boxShadow = '2px 8px 15px -6px red'
+    }
+  }
+
+  function hendleDelete(i){
+    setTag(
+    tag.filter((item,index)=>{
+      if(index !== i){
+        return true
+      }
+      return false
+      
+    }))
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div className='app'>
+      <div className="form">
+          <input type='text' value={text} onChange={hendleChange} onBlur={hendleOnBlur} />
+          <button disabled={text.length < 1 ? true : false} className={text.length < 1 ? 'off': 'on'} onClick={hendleSend}>Отправить</button>
+      </div>
+      <div className='error'>{error}</div>
+      <div className='tags-block'>
+
+        {newArr.map((item,index)=>{
+          return(
+          <div key={index}>{newArr[index]}<button onClick={()=>hendleDelete(index)}>x</button></div>
+          )
+        })}
+      </div>
     </div>
-  );
+    </>
+  )
 }
 
 export default App;
